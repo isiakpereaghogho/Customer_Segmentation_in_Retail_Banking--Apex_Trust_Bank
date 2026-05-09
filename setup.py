dd80df6d11297c08283414d99cdd3ca1aecd5bb3
isiakpereaghogho <85246849+isiakpereaghogho@users.noreply.github.com>
2026-05-10 00:29:25 +0100

    Exploratory data analysis

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+setup(
+    name = "apex_trust_bank_customer_segmentation",
+    version = "1.0.0",
+    author = "Aghogho Isiakpere",
+    packages = find_packages()
+)
